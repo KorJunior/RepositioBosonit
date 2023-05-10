ac78c5e55cb68516027db48d9944b5bf123804b0
Korodjouma Junior Fofana Coulibaly <junior.fofana@bosonit.com>
2023-05-10 16:31:20 +0200

    Primer Ejercicio

---
+++ block1-process-file-and-streams/src/InvalidLineFormatException.java
@@ -0,0 +1,15 @@
+public class InvalidLineFormatException extends Exception {
+
+    public InvalidLineFormatException(String message) {
+        super(message);
+    }
+
+    public InvalidLineFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidLineFormatException(Throwable cause) {
+        super(cause);
+    }
+}
+
