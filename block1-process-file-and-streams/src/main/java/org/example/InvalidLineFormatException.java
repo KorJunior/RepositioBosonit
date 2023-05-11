cecc4d26518608317cc07f3e724f43048deaed9e
Korodjouma Junior Fofana Coulibaly <junior.fofana@bosonit.com>
2023-05-11 09:50:33 +0200

    Esta es la buena

---
+++ block1-process-file-and-streams/src/main/java/org/example/InvalidLineFormatException.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class InvalidLineFormatException extends Exception {
+
+    public InvalidLineFormatException(String message) {
+        super(message);
+    }
+
+    public InvalidLineFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidLineFormatException(Throwable cause) {
+        super(cause);
+    }
+}
+
