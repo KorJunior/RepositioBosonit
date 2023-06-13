5739222689477bfa9a5c914da3ac2a727e3cf2fa
Korodjouma Junior Fofana Coulibaly <junior.fofana@bosonit.com>
2023-06-13 15:35:19 +0200

    Commit mongodb

--- block13-mongodb/src/main/java/com/example/block13mongodb/MongoDBConfig.java
+++ block13-mongodb/src/main/java/com/example/block13mongodb/MongoDBConfig.java
@@ -22,10 +22,12 @@ public class MongoDBConfig {
 
     @Bean
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://127.0.0.1:27017");
+        //ConnectionString connectionString = new ConnectionString("mongodb://127.0.0.1:27017");
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://junior:fofana@cluster0.kozhdlk.mongodb.net");
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
-                .credential(MongoCredential.createCredential("mdbUser", "myMongoDB", "cp".toCharArray()))
+                //Se comenta cuando se accede a remote a una base de Datos y no se comenta si es a Local
+                //.credential(MongoCredential.createCredential("mdbUser", "myMongoDB", "cp".toCharArray()))
                 .build();
 
         return MongoClients.create(settings);
